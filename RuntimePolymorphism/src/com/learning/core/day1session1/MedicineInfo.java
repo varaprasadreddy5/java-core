a902bd8ba26966e3dcb3fabaf7ba8b51fa25b7fd
varaprasadreddy5 <150122617+varaprasadreddy5@users.noreply.github.com>
2024-04-04 01:39:53 +0530

    Add files via upload

---
+++ RuntimePolymorphism/src/com/learning/core/day1session1/MedicineInfo.java
@@ -0,0 +1,5 @@
+package com.learning.core.day1session1;
+
+public interface MedicineInfo {
+    void displayLable();
+}
---
+++ RuntimePolymorphism/src/com/learning/core/day1session1/Tablet.java
@@ -0,0 +1,10 @@
+package com.learning.core.day1session1;
+
+
+public class Tablet implements MedicineInfo{
+
+    @Override
+    public void displayLable() {
+        System.out.println("Store Tablets in a cool dry place.");
+    }
+}
