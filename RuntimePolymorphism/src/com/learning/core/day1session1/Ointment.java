9f83ae17bdbdf30c0e8636b64187a04778c97e39
varaprasadreddy5 <150122617+varaprasadreddy5@users.noreply.github.com>
2024-04-04 01:49:27 +0530

    Problem_statement_2

---
+++ RuntimePolymorphism/src/com/learning/core/day1session1/Ointment.java
@@ -0,0 +1,9 @@
+package com.learning.core.day1session1;
+
+public class Ointment implements MedicineInfo{
+    @Override
+    public void displayLable() {
+        System.out.println("Ointment is for external use only.");
+
+    }
+}
